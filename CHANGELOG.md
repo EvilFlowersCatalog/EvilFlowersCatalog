8f421198582319c63bbbfe95038dbb119fdae599
Jakub Dubec <jakub.dubec@gmail.com>
2021-01-28 00:46:51 +0100

    Author have catalog 🙈

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## TBD
+
+- **Architecture**: Multiple catalogs database design
+- **Feature**: `Catalog` and `Feed` CRUD
--- apps/core/models/author.py
+++ apps/core/models/author.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext as _
 
+from apps.core.models.catalog import Catalog
 from apps.core.models.base import BaseModel
 
 
@@ -12,6 +13,7 @@ class Author(BaseModel):
         verbose_name = _('Author')
         verbose_name_plural = _('Authors')
 
+    catalog = models.ForeignKey(Catalog, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     surname = models.CharField(max_length=255)
 
