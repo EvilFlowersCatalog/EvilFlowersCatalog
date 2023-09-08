9437d3abb5acaf84507ed146f32110d5fd836891
Jakub Dubec <jakub.dubec@gmail.com>
2023-09-08 20:42:41 +0200

    Shelf records (codestyle) 😇

--- apps/api/forms/shelf_records.py
+++ apps/api/forms/shelf_records.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.db.models import Q
 from django_api_forms import Form
 
 from apps.core.models import Entry
@@ -7,9 +6,3 @@ from apps.core.models import Entry
 
 class ShelfRecordForm(Form):
     entry_id = forms.ModelChoiceField(queryset=Entry.objects.all())
-
-    # def clean_entry_id(self):
-    #     self.fields['entry_id'].queryset = self.fields['entry_id'].queryset.filter(
-    #         Q(catalog__in=self._request.user.catalogs) | Q(catalog__is_public=True)
-    #     )
-    #     return self.cleaned_data['entry_id']
