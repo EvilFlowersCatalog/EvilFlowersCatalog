6ec6fa84e90a41fc24532612a53bb9b3501238e8
Jakub Dubec <jakub.dubec@gmail.com>
2025-07-05 22:16:25 +0200

    Kafka poetry 🤫

--- requirements.sh
+++ requirements.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-poetry export -f requirements.txt --without-hashes --output requirements.txt --with s3 --with pdf --with apm --with docker
+poetry export -f requirements.txt --without-hashes --output requirements.txt --with s3 --with pdf --with apm --with docker --with kafka
